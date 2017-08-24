cf62a36fee1bc0ddbf3226d68c92bd703ef6a707
Christopher A Megown <megown1204@gmail.com>
2017-08-24 18:49:51 -0400

    working draft

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -5,14 +5,14 @@ public class FortuneTeller {
 	public static void main(String[] args) {
 		// Main variables
 		Scanner input = new Scanner(System.in);
-		System.out.println("I would like to reveal your fortune, but first I am going to need some information.");
+		System.out.println("I would like to reveal your fortune,\nbut first I am going to need some information.");
 		System.out.println("What is your first name?");
 		String firstName = input.next();
 		System.out.println("What is your last name?");
 		String lastName = input.next();
 		System.out.println("How old are you? (In years plz)");
 		int ageOfPerson = input.nextInt();
-		System.out.println("What month were you born in? (January=1, February=2 etc...");
+		System.out.println("What month were you born in? (1-12)");
 		int monthBorn = input.nextInt();
 		System.out.println("What is your favorite ROYGBIV color? (not sure what ROYGBIV means...type HELP)");
 		String favColor = input.next();
@@ -43,23 +43,27 @@ public class FortuneTeller {
 		} else if (siblingCount > 3) {
 			vacationLocation = "Salt Lake City, Utah";
 		} else if (siblingCount < 0) {
-			vacationLocation = "Downtown Detroit";
+			vacationLocation = "Downtown Detroit...in a box";
 		}
 
 		// Mode of Transportation
 		String modeOfTransportation = null;
-		
-		if (favColor.equals("red")) {
-			modeOfTransportation = "Corvette";
-		} else if (favColor.equals("orange")) {
-			modeOfTransportation = "Oscar Mayer Weinermobile";
-		} else if (favColor.equals("yellow")) {
-			modeOfTransportation = "Jeep Wrangler";
-		} else if (favColor.equals("green")) {
-			modeOfTransportation = "Jaguar";
-		} else if (favColor.equals("blue")) {
-			modeOfTransportation = "Private Jet";
-		} else
+
+		if (favColor.toLowerCase().equals("red")) {
+			modeOfTransportation = " Golden Segway";
+		} else if (favColor.toLowerCase().equals("orange")) {
+			modeOfTransportation = "n Oscar Mayer Weinermobile";
+		} else if (favColor.toLowerCase().equals("yellow")) {
+			modeOfTransportation = " Jeep Wrangler";
+		} else if (favColor.toLowerCase().equals("green")) {
+			modeOfTransportation = " Tank";
+		} else if (favColor.toLowerCase().equals("blue")) {
+			modeOfTransportation = " Private Jet";
+		} else if (favColor.toLowerCase().equals("indigo")) {
+			modeOfTransportation = " Tamed Lion named Stu";
+		} else if (favColor.toLowerCase().equals("violet")) {
+			modeOfTransportation = " Jetpack";
+		}
 
 		// Bank Balance
 		double endingBankBalance = 0;
@@ -75,8 +79,8 @@ public class FortuneTeller {
 
 		// The end result
 		System.out.println(firstName + " " + lastName + " will retire in " + retirementYears + " years with $"
-				+ endingBankBalance + ", a vacation home in " + vacationLocation
-				+ ", and will move about the earth via " + modeOfTransportation);
+				+ endingBankBalance + ",\na vacation home in " + vacationLocation
+				+ ", \nand will move about the earth via a" + modeOfTransportation + "!!");
 
 	}
 
